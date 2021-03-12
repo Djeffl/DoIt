61f958ecca8d6af6bff48d4bc1d9d005cf736d5e
jeffl <liekensjeff@gmail.com>
2021-03-13 00:33:12 +0100

    Move API to .NET 5; Move Blazor webassembly to .NET 5; Mock services in web UI;

--- DoIt.Client/Program.cs
+++ DoIt.Client/Program.cs
@@ -1,23 +1,51 @@
+using DoIt.Client.Services;
+using DoIt.Client.Services.Mocks;
+using DoIt.ComponentLibrary;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using DoIt.ComponentLibrary;
 
 namespace DoIt.Client
 {
-	public class Program
-	{
-		public static async Task Main(string[] args)
-		{
-			var builder = WebAssemblyHostBuilder.CreateDefault(args);
-			builder.RootComponents.Add<App>("app");
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
 
-			builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["DoIt.Api.Url"]) });
-			builder.Services.AddModal();
+            AddCoreServices(builder);
 
-			await builder.Build().RunAsync();
-		}
-	}
+            if (builder.HostEnvironment.IsDevelopment())
+            {
+                AddMockingServices(builder);
+            } else
+            {
+                AddImplementedServices(builder);
+            }
+
+            
+
+            await builder.Build().RunAsync();
+        }
+
+        private static void AddCoreServices(WebAssemblyHostBuilder builder)
+        {
+            builder.Services.AddModal();
+        }
+
+        private static void AddImplementedServices(WebAssemblyHostBuilder builder)
+        {
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(typeof(ITodoService), typeof(TodoService));
+        }
+
+        private static void AddMockingServices(WebAssemblyHostBuilder builder)
+        {
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(typeof(ITodoService), typeof(TodoServiceMock));
+        }
+    }
 }
