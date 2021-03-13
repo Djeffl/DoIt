c8a83c140289998ef892d799dce2d880b9c59426
jeffl <liekensjeff@gmail.com>
2021-03-13 01:11:21 +0100

    Add web api security (note: this will not work as we have a standalone blazor application);

---
+++ DoIt.Api/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoIt.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("GoogleSignIn")]
+        public async Task GoogleSignIn()
+        {
+            await HttpContext.ChallengeAsync(GoogleDefaults.AuthenticationScheme, new AuthenticationProperties { RedirectUri = "/profile" });
+        }
+    }
+}
--- DoIt.Api/Startup.cs
+++ DoIt.Api/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using DoIt.Api.Data;
 using DoIt.Api.Extensions;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -19,16 +20,26 @@ namespace DoIt.Api
 {
 	public class Startup
 	{
+		public IConfiguration Configuration { get; }
+
 		public Startup(IConfiguration configuration)
 		{
 			Configuration = configuration;
 		}
 
-		public IConfiguration Configuration { get; }
-
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddAuthentication(OptionsBuilderConfigurationExtensions =>
+			{
+				OptionsBuilderConfigurationExtensions.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+			}).AddCookie()
+			.AddGoogle(googleOptions =>
+			{
+				googleOptions.ClientId = Configuration["Authentication:Google:ClientId"];
+				googleOptions.ClientSecret = Configuration["Authentication:Google:ClientSecret"];
+			});
+
 			services.AddMvc();
 
 			services.AddControllers();
