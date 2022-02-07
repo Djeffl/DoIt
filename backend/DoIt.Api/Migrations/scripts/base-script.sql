668cec9a538dc7a4a51f10d7a2121c97538c6145
Jeff Liekens <jeff.liekens@euri.com>
2022-02-08 00:01:05 +0100

    update base-script;

--- backend/DoIt.Api/Migrations/scripts/base-script.sql
+++ backend/DoIt.Api/Migrations/scripts/base-script.sql
@@ -5,6 +5,20 @@ IF NOT EXISTS(
         sys.databases
     WHERE
         name = 'DoIt'
-) BEGIN CREATE DATABASE DoIt
-Exec sp_defaultdb @loginame='sa', @defdb='DoIt' 
-END
+) BEGIN 
+	CREATE DATABASE DoIt
+END;
+
+USE DoIt
+
+CREATE USER DeffUser
+	FOR LOGIN DeffUser
+	WITH DEFAULT_SCHEMA = dbo
+GO;
+
+-- Add user to the database owner role
+EXEC sp_addrolemember N'db_owner', N'DeffUser'
+GO;
+
+Exec sp_defaultdb @loginame='DeffUser', @defdb='DoIt' 
+GO;
