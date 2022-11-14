a998f52b0497f5679ad0abbb12ed9ad819353253
Jeff Liekens <jeff.liekens@euri.com>
2022-11-14 23:05:19 +0100

    Add Calendar stuff;

--- frontend/DoIt.Client/Extensions/DateFormatExtensions.cs
+++ frontend/DoIt.Client/Extensions/DateFormatExtensions.cs
@@ -9,5 +9,17 @@ namespace DoIt.Client.Extensions
         {
             return date.ToString("d", CultureInfo.CreateSpecificCulture("de-DE"));
         }
+
+        public static string ToStringFormat(this DayOfWeek dayOfWeek)
+        {
+            CultureInfo english = new CultureInfo("en-US");
+            return (english.DateTimeFormat.DayNames[(int)dayOfWeek]).Substring(0, 2);
+        }
+
+        public static string ToDayOfMonthStringFormat(this DateTime date)
+        {
+            CultureInfo english = new CultureInfo("en-US");
+            return (english.DateTimeFormat.MonthNames[(int)date.Month-1]);
+        }
     }
 }
